ffd4403767a243fc6515d889085ff0939d9b1303
Yana Stepanchenko <117828836+yanastepan@users.noreply.github.com>
2024-06-27 17:56:20 -0600

    The final task solution

---
+++ homework_3-1.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+
+def get_days_from_today(date) -> int:
+
+    # converting date type from string to datetime using strptime method
+    # checking if data was typed in correct format:
+    
+    try:
+        converted_date = datetime.strptime(date, "%Y-%m-%d").date()
+    except ValueError:
+        print("Wrong date type. Should be written in a YYYY-MM-DD format. Please, try again")
+        return None  
+    
+    # Return None to indicate that an error occurred
+
+    # finding current day:
+    
+    today = datetime.today().date()
+
+    # counting difference between current day (today) and converted typed in date (converted_date)
+    # converting them from datetime objects into integer using toordinal method
+    # that way we count the difference between the ordinal numbers of the dates:
+
+    difference = converted_date.toordinal() - today.toordinal()
+
+    return difference
+
+''' # example usage:
+date = input("Please, write a date: ")
+print(f"The difference between the current day and your date is {get_days_from_today(date)}")
+'''
